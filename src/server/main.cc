9fdb39b1ff8871e911144fc97ac66f428d94d640
František Bořánek <frantisek.boranek@firma.seznam.cz>
2020-07-24 11:20:53 +0200

    feat: added some server options

--- src/server/main.cc
+++ src/server/main.cc
@@ -1,5 +1,6 @@
 #include "server.grpc.pb.h"
 #include <csignal>
+#include <cstdlib>
 #include <grpcpp/grpcpp.h>
 
 class TestServiceImpl final : public TestService::Service {
@@ -12,6 +13,27 @@ public:
         return {};
     }
 };
+
+class EnvServerOption : public grpc::ServerBuilderOption {
+public:
+    void UpdateArguments(grpc::ChannelArguments* args) override
+    {
+        auto setInt = [&](const std::string& name) {
+            std::string envName = name;
+            std::replace(envName.begin(), envName.end(), '.', '_');
+            const char* strVal = std::getenv(envName.c_str());
+            if (strVal) {
+                auto val = std::stoi(strVal);
+                args->SetInt(name, val);
+                std::cout << "GRPC server option " << name << ": " << val << std::endl;
+            }
+        };
+        setInt(GRPC_ARG_MAX_CONNECTION_AGE_MS);
+        setInt(GRPC_ARG_MAX_CONNECTION_AGE_GRACE_MS);
+    }
+    void UpdatePlugins(std::vector<std::unique_ptr<grpc::ServerBuilderPlugin>>*) override { }
+};
+
 
 std::unique_ptr<grpc::Server> grpcServer;
 
@@ -28,9 +50,10 @@ try {
         builder.SetSyncServerOption(grpc::ServerBuilder::MAX_POLLERS, 100);
         builder.SetSyncServerOption(grpc::ServerBuilder::NUM_CQS, 1);
         builder.SetSyncServerOption(grpc::ServerBuilder::CQ_TIMEOUT_MSEC, 10'000);
+        builder.SetOption(std::make_unique<EnvServerOption>());
         builder.RegisterService(&testServiceImpl);
 
-        std::cout << "GRPC server started listening at " << grpcUri << std::endl;
+        std::cout << "GRPC server configured listening at " << grpcUri << std::endl;
         return builder.BuildAndStart();
     }();
 
